bdc12cfa1475dd145eacf24b6f43324e804b9026
Vanessa <vanessasouzaoliveira1@gmail.com>
2021-04-23 23:59:17 -0300

    create confirmation page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 
-import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
  
 
 import {
@@ -22,7 +22,7 @@ export default function App(){
 		
 
 	return (
-		<UserIdentification />
+		<Confirmation />
 	)
 }
 
