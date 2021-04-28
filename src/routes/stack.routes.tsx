db36e614adc7798dc8824b896b7dc8b3423db417
Vanessa <vanessasouzaoliveira1@gmail.com>
2021-04-28 13:20:19 -0300

    create link with api

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,9 +4,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect'
+import { PlantSave } from '../pages/PlantSave';
 
 import colors from '../styles/colors';
+import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
+
 ;
 
 const stackRoutes = createStackNavigator();
@@ -38,9 +41,18 @@ const AppRoutes: React.FC = () => (
 
         <stackRoutes.Screen
             name="PlantSelect"
-            component={PlantSelect}
+            //tirei o plantSelect
+            component={AuthRoutes}
         />
         
+        <stackRoutes.Screen
+            name="PlantSave"
+            component={PlantSave}
+        />
+        <stackRoutes.Screen
+            name="MyPlants"
+            component={AuthRoutes}
+        />
     </stackRoutes.Navigator>
 
 )
